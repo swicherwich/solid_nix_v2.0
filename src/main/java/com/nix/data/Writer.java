b5973e7190471e165ede9696bb8341ec5b7ce55d
Swicherwich <gitgilfoyle@gmail.com>
2020-06-30 13:23:59 +0300

    avoided using new

--- src/main/java/com/nix/data/Writer.java
+++ src/main/java/com/nix/data/Writer.java
@@ -1,14 +1,20 @@
 package com.nix.data;
 
+import com.nix.config.ObjectFactory;
 import com.nix.service.Inspiration;
 import com.nix.service.WritingPreparation;
 import com.nix.service.impl.InspirationImpl;
 import com.nix.service.impl.WritingPreparationImpl;
 
+import java.lang.reflect.InvocationTargetException;
+
 public class Writer {
 	
-	private final Inspiration inspiration = new InspirationImpl();
-	private final WritingPreparation writingPreparation = new WritingPreparationImpl();
+	private final Inspiration inspiration = ObjectFactory.getInstance().createObject(Inspiration.class);
+	private final WritingPreparation writingPreparation = ObjectFactory.getInstance().createObject(WritingPreparation.class);
+	
+	public Writer() throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+	}
 	
 	public void start(Idea idea) {
 		inspiration.hasInspiration();
